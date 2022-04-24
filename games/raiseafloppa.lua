d4d0b4bf014ee1317db36fe518c321e018a02b53
Rikki <ikas.noa@gmail.com>
2022-04-24 19:21:52 +0000

    auto refill

--- games/raiseafloppa.lua
+++ games/raiseafloppa.lua
@@ -1,7 +1,11 @@
 Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/UnseenRikk/StupidGarbage/main/LinoriaRX.lua'))();
 
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+local Players = game:GetService("Players");
+local LocalPlayer = Players.LocalPlayer;
+local ShopMenu = LocalPlayer.PlayerGui.Shop.Frame.ScrollingFrame
+local BowlPart = game.Workspace.Bowl.Part
+local Money = LocalPlayer.leaderstats.Money
+local PlayerName = LocalPlayer.Name
 
 local MainWindow = Library:CreateWindow('Raise A Floppa');
 
@@ -11,6 +15,7 @@ local hacktabbox = hacktab:AddLeftTabbox();
 local hack = hacktabbox:AddTab('hack');
 hack:AddToggle('AutoClicker', { Text = 'Auto clicker' });
 hack:AddToggle('AutoPickupMoney', { Text = 'Auto pickup money' });
+hack:AddToggle('AutoRefill', { Text = 'Auto refill bowl' });
 
 task.spawn(function()
     while game:GetService('RunService').RenderStepped:Wait() do
@@ -27,6 +32,36 @@ task.spawn(function()
             for i,v in pairs(workspace:GetChildren()) do
                 if v.Name == "Money" or v.Name == "Money Bag" then
                     firetouchinterest(LocalPlayer.Character.HumanoidRootPart, v, 0)
+                end;
+            end;
+        end;
+    end;
+end);
+
+local function HasFood()
+    for i,v in pairs(game.Workspace:GetChildren()) do
+        if v.Name == PlayerName then
+            for i,v2 in pairs(v:GetChildren()) do
+                if v2.Name == "Floppa Food" then
+                    return true
+                end;
+            end;
+        else
+            return false
+        end;
+    end;
+end;
+
+task.spawn(function()
+    while game:GetService('RunService').RenderStepped:Wait() do
+        if Toggles.AutoRefill.Value then
+            if BowlPart.Transparency == 1 then
+                if Money.Value < 50 and HasFood() == false then
+                else
+                    for i,v in pairs(getconnections(ShopMenu.Food.Purchase.MouseButton1Click)) do
+                        v:Fire()
+                    end;
+                    fireproximityprompt(BowlPart.ProximityPrompt, 0)
                 end;
             end;
         end;
